0de27684de5c7daa89afc83c36147eae070ed661
ATesic49 <atesic7@gmail.com>
2024-02-13 01:04:09 +0100

    das

--- app/components/ONama.tsx
+++ app/components/ONama.tsx
@@ -12,16 +12,16 @@ function ONama() {
             </h2>
             <div className='flex justify-between px-4  items-center w-full '>
                 <p className='text-gray-500 text-lg max-w-[30em]'>
-                    Što se nas tiče, moramo da vam odamo jednu tajnu... <strong className='text-gray-800'>Imamo samo 18 godina.</strong>
+                    Što se nas tiče, moramo da vam odamo jednu tajnu... <strong className='text-gray-700'>Imamo samo 18 godina.</strong>
                     <br />
                     <br />
                     Naš tim sastoji se iz svega dvoje ljudi, od kojih je jedan zadužen za izradu web sajtova, a drugi za marketing.
                     <br />
                     <br />
-                    Ne želimo da se pravimo da smo neka ogromna firma, jer vama to <strong className='text-gray-800'> u suštini i ne treba.</strong>
+                    Ne želimo da se pravimo da smo neka ogromna firma, jer vama to <strong className='text-gray-700'> u suštini i ne treba.</strong>
                     <br />
                     <br />
-                    Vama samo trebaju ljudi koji zaista žele da vam ponognu da <strong className='text-gray-800'> ostvarite svoj cilj </strong>, pre nego da vam samo uzmu novac, i prekopiraju tudji sajt čisto da bi vas 'otarasili'.
+                    Vama samo trebaju ljudi koji zaista žele da vam ponognu da <strong className='text-gray-700'> ostvarite svoj cilj </strong>, pre nego da vam samo uzmu novac, i prekopiraju tudji sajt čisto da bi vas &apos;otarasili&apos;.
                 </p>
                 <Image src={image} alt='' className='w-1/3 rounded-t-2xl  aspect-square  rounded'></Image>
 
