a526b37a78d7ad849eab36a704fec15bfd24b757
ATesic49 <atesic7@gmail.com>
2024-02-13 01:09:38 +0100

    da

--- app/components/MobileNavbar.tsx
+++ app/components/MobileNavbar.tsx
@@ -5,50 +5,39 @@ function MobileNavbar() {
     return (
         <div className='fixed w-full bg-white bottom-0 border-t z-50 sm:hidden grid grid-cols-4 justify-center items-center'>
 
+            <Link href="/o-nama" className='text-graya-900 text-blue-400 bg-red-200 flex justify-center items-center text-center text-[10px] flex-col gap-1 w-full py-2 '>
+
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-8  stroke-blue-400 fill-blue-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg>
+                O nama</Link>
             <Link href="/o-nama" className='text-graya-900 text-blue-400 border-r flex justify-center items-center text-center text-[10px] flex-col gap-1 w-full py-2 '>
 
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-5  stroke-blue-400 fill-blue-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-8 stroke-blue-400 fill-blue-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg>
+                O nama</Link>
+            <Link href="/o-nama" className='text-graya-900 text-blue-400 border-r flex justify-center items-center text-center text-[10px] flex-col gap-1 w-full py-2 '>
+
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-8  stroke-blue-400 fill-blue-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg>
+                O nama</Link>
+            <Link href="/o-nama" className='text-graya-900 text-blue-400 border-r flex justify-center items-center text-center text-[10px] flex-col gap-1 w-full py-2 '>
+
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-8  stroke-blue-400 fill-blue-400" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
                     <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
                 </svg>
                 O nama</Link>
 
-            <Link href="/o-nama" className='text-gray-900 border-r flex justify-center items-center text-center text-[10px] flex-col gap-1 w-full py-2 '>
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4  fill-gray-900" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
-                    <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M17 10h2a2 2 0 0 1 2 2v1" />
-                    <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
-                </svg>
-                O nama</Link>
-
-            <Link href="/o-nama" className='text-gray-900 border-r flex justify-center items-center text-center text-[10px] flex-col gap-1 w-full py-2 '>
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4  stroke-gray-900" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
-                    <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M17 10h2a2 2 0 0 1 2 2v1" />
-                    <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
-                </svg>
-                O nama</Link>
-
-            <Link href="/o-nama" className='text-gray-900 border-r flex justify-center items-center text-center text-[10px] flex-col gap-1 w-full py-2 '>
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-4  stroke-gray-900" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M10 13a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M8 21v-1a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v1" />
-                    <path d="M15 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M17 10h2a2 2 0 0 1 2 2v1" />
-                    <path d="M5 5a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                    <path d="M3 13v-1a2 2 0 0 1 2 -2h2" />
-                </svg>
-                O nama</Link>
 
 
         </div>
