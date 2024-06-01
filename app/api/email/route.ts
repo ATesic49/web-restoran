2f9371a101c65be08bc4622540d7fe9e2d8ff437
ATesic49 <atesic7@gmail.com>
2024-06-01 16:26:02 +0200

    dsadasd

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
 
 export async function POST(request: Request) {
+  console.log("POSLAT JE REQUEST");
   const { ime, email, poruka } = await request.json();
 
   var transporter = nodemailer.createTransport({
@@ -17,18 +18,20 @@ export async function POST(request: Request) {
     subject: `Stigla je poruka od ${ime} , ${email}`,
     text: poruka,
   };
+  console.log("EMAIL OPCIJE RADE");
   try {
     const mail = await transporter.sendMail(
       mailOptions,
       function (error: any, info: any) {
         if (error) {
           console.log(error);
+          console.log("NEGDE JE DOSLO DO GRESKE");
         } else {
           console.log("Email sent: " + info.response);
         }
       }
     );
-
+    console.log(mail);
     return Response.json({ status: "Sve je proslo kako treba", mail: mail });
   } catch (e) {
     console.log(e);
