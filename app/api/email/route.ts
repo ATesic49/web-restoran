3db09bcc9432ec00b5fa7cb3ddc9ad949971cbe9
ATesic49 <atesic7@gmail.com>
2024-06-11 00:58:44 +0200

    ADSasd

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,31 +1,31 @@
 import nodemailer from "nodemailer";
 
 export async function POST(request: Request) {
-  console.log("POSLAT JE REQUEST");
-  const { ime, email, poruka } = await request.json();
+	console.log("POSLAT JE REQUEST");
+	const { ime, email, poruka } = await request.json();
 
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "atesic7@gmail.com",
-      pass: "igea elti hugs heys",
-    },
-  });
+	var transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: "atesic7@gmail.com",
+			pass: "igea elti hugs heys",
+		},
+	});
 
-  var mailOptions = {
-    from: email,
-    to: "atesic7@gmail.com",
-    subject: `Stigla je poruka od ${ime} , ${email}`,
-    text: poruka,
-  };
-  console.log("EMAIL OPCIJE RADE");
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent: " + info.response);
-    return Response.json({ status: "Sve je prošlo kako treba." });
-  } catch (e: any) {
-    console.log("Negde je doslo do greske");
-    console.error(e.message);
-    return Response.json({ status: "greska" });
-  }
+	var mailOptions = {
+		from: email,
+		to: "atesic7@gmail.com",
+		subject: `Stigla je poruka od ${ime} , ${email}`,
+		text: poruka,
+	};
+	console.log("EMAIL OPCIJE RADE");
+	try {
+		const info = await transporter.sendMail(mailOptions);
+		console.log("Email sent: " + info.response);
+		return Response.json({ status: "Email uspešno poslat." });
+	} catch (e: any) {
+		console.log("Negde je doslo do greske");
+		console.error(e.message);
+		return Response.json({ status: "greska" });
+	}
 }
--- app/kontakt/components/Kontakt.tsx
+++ app/kontakt/components/Kontakt.tsx
@@ -1,128 +1,146 @@
-'use client'
-import axios from 'axios'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { CircularProgress } from '@mui/material'
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import React, { useState } from "react";
+import { CircularProgress } from "@mui/material";
 function Kontakt() {
-    const [emailState, setEmailState] = useState({
-        ime: '',
-        email: '',
-        poruka: ''
-    })
-    const [status, setStatus] = useState('')
-    const handleSubmit = async () => {
-        if (emailState.ime.length < 5 || emailState.email.length < 12 || emailState.poruka.length < 25) return setStatus('Molimo Vas popunite polja do kraja.')
-        else {
-            try {
+	const [emailState, setEmailState] = useState({
+		ime: "",
+		email: "",
+		poruka: "",
+	});
+	const [status, setStatus] = useState("");
+	const handleSubmit = async () => {
+		if (
+			emailState.ime.length < 5 ||
+			emailState.email.length < 12 ||
+			emailState.poruka.length < 15
+		)
+			return setStatus("Molimo Vas popunite polja do kraja.");
+		else {
+			try {
+				setStatus("circ");
+				const res = await axios.post("/api/email", { ...emailState });
+				console.log(res);
+				if (res.data.status) {
+					setStatus(res.data.status);
+					setTimeout(() => {
+						setStatus("");
+					}, 2000);
+				} else {
+					throw new Error("Negde je došlo do greške");
+				}
+			} catch (e: any) {
+				console.log(e);
+				setStatus(e.message);
+				setTimeout(() => {
+					setStatus("");
+				}, 2000);
+			}
+		}
+	};
+	return (
+		<div className="flex py-8 flex-col justify-center gap-8 items-center min-h-screen   relative bg-gray-950 text-gray-400 z-10 overflow-hidden pt-24">
+			<div className="absolute inset-0 grid grid-cols-[repeat(20,128px)] md:grid-cols-[repeat(20,256px)] divide-x-2  divide-gray-800 -z-10 overflow-hidden">
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+				<div className="w-full h-full"></div>
+			</div>
+			<div className="absolute bg-gradient-to-t from-blue-400 to-blue-200 opacity-25 -z-10 blur-lg top-1/3 w-1/3 left-1/2 h-1/2 ">
+				{" "}
+			</div>
+			<div className="absolute bg-gradient-to-t from-pink-400 to-pink-100 opacity-10 -z-10 blur-lg aspect-square  top-1/2 -translate-y-1/2 rounded-full  left-0  w-1/3 ">
+				{" "}
+			</div>
 
-                setStatus('circ')
-                const res = await axios.post('/api/email', { ...emailState })
-                console.log(res)
-                if (res.data.status) {
-                    setStatus(res.data.status)
-                    setTimeout(() => { setStatus('') }, 2000)
-                } else {
-                    throw new Error('Negde je došlo do greške')
-                }
-            } catch (e: any) {
-                console.log(e)
-                setStatus(e.message)
-                setTimeout(() => { setStatus('') }, 2000)
+			<h1 className="md:text-4xl text-2xl w-5/6 text-center md:w-auto md:text-left text-gray-400 font-bold">
+				Pozovi nas, ili zakazi sastanak
+			</h1>
+			<div className={`-my-4 text-green-500 `}>
+				{status === "Molimo Vas popunite polja do kraja." ? (
+					<div className="text-red-500">{status}</div>
+				) : (
+					<>{status === "circ" ? <CircularProgress /> : <>{status}</>}</>
+				)}
+			</div>
 
+			<div className="grid text-white grid-cols-2 relative gap-x-16 gap-y-8 bg-gradient-to-tr from-blue-500 to-pink-300 p-8 pb-4 rounded mx-4 shadow-md shadow-blue-950">
+				<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[calc(100%_-_2rem)] w-[1px] md:bg-gray-300"></div>
 
-            }
-        }
+				<div className="w-full md:col-span-1 col-span-2 flex flex-col gap-4 ">
+					<div className="flex w-full flex-col justify-start items-start gap-1">
+						<p className="text-sm">Ime i Prezime:</p>
 
-    }
-    return (
-        <div className='flex py-8 flex-col justify-center gap-8 items-center min-h-screen   relative bg-gray-950 text-gray-400 z-10 overflow-hidden pt-24'>
-            <div className='absolute inset-0 grid grid-cols-[repeat(20,128px)] md:grid-cols-[repeat(20,256px)] divide-x-2  divide-gray-800 -z-10 overflow-hidden'>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-                <div className='w-full h-full'></div>
-            </div>
-            <div className='absolute bg-gradient-to-t from-blue-400 to-blue-200 opacity-25 -z-10 blur-lg top-1/3 w-1/3 left-1/2 h-1/2 '> </div>
-            <div className='absolute bg-gradient-to-t from-pink-400 to-pink-100 opacity-10 -z-10 blur-lg aspect-square  top-1/2 -translate-y-1/2 rounded-full  left-0  w-1/3 '> </div>
+						<input
+							onChange={(e) => {
+								setEmailState({
+									...emailState,
+									ime: e.target.value,
+								});
+							}}
+							type="text"
+							className="p-2 rounded text-sm outline-none w-full md:w-[20em] text-gray-600 bg-gray-200 "
+						/>
+					</div>
+					<div className="flex flex-col justify-start items-start gap-1">
+						<p className="text-sm">Email:</p>
 
-            <h1 className='md:text-4xl text-2xl w-5/6 text-center md:w-auto md:text-left text-gray-400 font-bold'>Pozovi nas, ili zakazi sastanak</h1>
-            <div className={`-my-4 text-green-500 `}>
+						<input
+							type="text"
+							className="p-2 rounded text-sm outline-none w-full md:w-[20em] text-gray-600 bg-gray-200"
+							onChange={(e) => {
+								setEmailState({
+									...emailState,
+									email: e.target.value,
+								});
+							}}
+						/>
+					</div>
+				</div>
 
-                {status === 'Molimo Vas popunite polja do kraja.' ? <div className='text-red-500'>{status}</div> : <>
+				<div className="w-full  md:col-span-1 col-span-2  flex flex-col gap-1 md:mt-0 -mt-4">
+					<p className="text-sm ">Poruka:</p>
 
-                    {status === 'circ' ? <CircularProgress /> : <>{status}</>}
-                </>}
-            </div>
+					<textarea
+						onChange={(e) => {
+							setEmailState({
+								...emailState,
+								poruka: e.target.value,
+							});
+						}}
+						name=""
+						className="w-full rounded h-full min-h-[7.5em] outline-none  text-sm p-2 text-gray-600 bg-gray-200"
+						id=""
+						defaultValue={""}
+					></textarea>
+				</div>
+				<div
+					className="px-4 col-span-2 cursor-pointer py-2 rounded  ml-auto text-gray-800 text-sm w-fit md:-mt-4 bg-gray-50   "
+					onClick={handleSubmit}
+				>
+					{" "}
+					Pošalji
+				</div>
+			</div>
 
-
-
-
-
-
-
-
-
-
-
-            <div className='grid text-white grid-cols-2 relative gap-x-16 gap-y-8 bg-gradient-to-tr from-blue-500 to-pink-300 p-8 pb-4 rounded mx-4 shadow-md shadow-blue-950'>
-
-                <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[calc(100%_-_2rem)] w-[1px] md:bg-gray-300'></div>
-
-
-
-                <div className='w-full md:col-span-1 col-span-2 flex flex-col gap-4 '>
-                    <div className='flex w-full flex-col justify-start items-start gap-1'>
-                        <p className='text-sm'>Ime i Prezime:</p>
-
-                        <input onChange={e => {
-                            setEmailState({
-                                ...emailState,
-                                ime: e.target.value
-                            })
-                        }} type="text" className='p-2 rounded text-sm outline-none w-full md:w-[20em] text-gray-600 bg-gray-200 ' />
-                    </div>
-                    <div className='flex flex-col justify-start items-start gap-1'>
-                        <p className='text-sm'>Email:</p>
-
-                        <input type="text" className='p-2 rounded text-sm outline-none w-full md:w-[20em] text-gray-600 bg-gray-200' onChange={e => {
-                            setEmailState({
-                                ...emailState,
-                                email: e.target.value
-                            })
-                        }} />
-                    </div>
-                </div>
-
-                <div className='w-full  md:col-span-1 col-span-2  flex flex-col gap-1 md:mt-0 -mt-4'>
-                    <p className='text-sm '>Poruka:</p>
-
-                    <textarea
-
-                        onChange={e => {
-                            setEmailState({
-                                ...emailState,
-                                poruka: e.target.value
-                            })
-                        }}
-                        name="" className='w-full rounded h-full min-h-[7.5em] outline-none  text-sm p-2 text-gray-600 bg-gray-200' id="" defaultValue={''}></textarea>
-                </div>
-                <div className='px-4 col-span-2 cursor-pointer py-2 rounded  ml-auto text-gray-800 text-sm w-fit md:-mt-4 bg-gray-50   ' onClick={handleSubmit}> Pošalji</div>
-            </div>
-
-            <p className='-my-4 text-gray-400'>ili</p>
-            <Link href={'tel:+381644737375'} className='p-2 rounded flex justify-center border-gray-400 text-gray-300 items-center border-2 text-sm   px-4 py-2'>
-                0644737375
-            </Link>
-        </div>
-    )
+			<p className="-my-4 text-gray-400">ili</p>
+			<Link
+				href={"tel:+381644737375"}
+				className="p-2 rounded flex justify-center border-gray-400 text-gray-300 items-center border-2 text-sm   px-4 py-2"
+			>
+				0644737375
+			</Link>
+		</div>
+	);
 }
 
-export default Kontakt
+export default Kontakt;
